b1f3a1ee41cf37a28609eae4096865d9f51442fc
AntonioShut <anshut@mail.ru>
2026-05-14 10:59:51 +0300

    Исправленна ошибка с прорисовкой комнат. Добавленны новые комнат первого этажа, новые спрайты.

--- game.py
+++ game.py
@@ -8,25 +8,52 @@ pygame.init()
 CELL_SIZE = 16
 WIDTH, HEIGHT = 800, 800
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Dungeon of Death")
+pygame.display.set_caption("Dungeon of Death_Pre-Alpha")
 '''
 1 - стена
 0 - пустота
 '''
 room_map = [
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 3, 1, 3, 1, 1, 3, 1, 1, 1, 3, 3, 3],
+    [3, 4, 0, 0, 0, 4, 3, 4, 4, 1, 0, 4, 3],
+    [3, 4, 0, 0, 0, 4, 1, 4, 0, 3, 3, 4, 1],
+    [3, 4, 4, 0, 0, 0, 2, 4, 0, 0, 2, 0, 1],
+    [1, 4, 0, 0, 0, 4, 3, 0, 4, 3, 1, 0, 1],
+    [1, 0, 0, 0, 4, 0, 3, 0, 4, 1, 0, 0, 2],
+    [1, 3, 3, 1, 1, 1, 1, 3, 3, 3, 1, 3, 3],
+]
+room_smal = [
+    [1, 1, 3, 3, 1, 1, 1, 1],
+    [0, 0, 0, 0, 4, 0, 0, 2],
+    [0, 4, 0, 0, 0, 0, 0, 3],
+    [0, 4, 4, 0, 0, 0, 4, 1],
+    [3, 0, 0, 0, 0, 4, 4, 1],
+    [1, 0, 4, 0, 0, 4, 4, 1],
+    [1, 1, 1, 3, 3, 3, 3, 1],
+]
+corridor_odin = [
+    [3, 1],
+    [4, 2],
+    [0, 3],
+    [4, 3],
+    [0, 1],
+    [0, 0],
+    [1, 2],
+]
+corridor_dlin = [
+    [3, 3, 3, 1, 1, 1, 1, 1, 3, 1],
+    [4, 0, 0, 4, 0, 0, 0, 0, 4, 2],
+    [1, 1, 1, 3, 2, 3, 3, 1, 1, 3],
+]
+corridor_vniz = [
+    [1, 4, 3],
+    [3, 4, 1],
+    [3, 0, 1],
+    [1, 0, 3],
+    [1, 0, 3],
+    [1, 0, 2],
+    [1, 4, 1],
+    [3, 3, 1],
 ]
 
 # Цвета
@@ -35,17 +62,55 @@ BLUE = (0, 0, 255)
 BLACK = (0, 0, 0)
 RED = (255, 0, 0)
 
-# Задаем смещение всей комнаты (в пикселях)
-OFFSET_X = 9
-OFFSET_Y = 9
+# Задаем смещение всех комнат (в пикселях)
+OFFSET_X = 0
+OFFSET_Y = 0
+
+room_smal_offset_x = len(room_map[0]) * CELL_SIZE + 32
+room_smal_offset_y = len(room_map) * CELL_SIZE
+
+corridor_odin_x = len(room_map[0]) * CELL_SIZE
+corridor_odin_y = len(room_map) * CELL_SIZE
+
+corridor_dlin_x = len(room_map[0]) * CELL_SIZE + 160
+corridor_dlin_y = len(room_map) * CELL_SIZE
+
+corridor_vniz_x = len(room_map[0]) * CELL_SIZE
+corridor_vniz_y = len(room_map) * CELL_SIZE + 112
+
+#ОТРИСОВКА КОМНАТ
+room = [
+    (room_map, 0, 112),
+    (room_smal, room_smal_offset_x, room_smal_offset_y),
+    (corridor_odin, corridor_odin_x, corridor_odin_y),
+    (corridor_dlin, corridor_dlin_x, corridor_dlin_y),
+    (corridor_vniz, corridor_vniz_x, corridor_vniz_y),
+]
+
 
 '''
-player_sprite = pygame.image.load(...): Загружает изображение игрока.
+sprite = pygame.image.load(...): Загружает изображение спрайта.
 
 '''
-player_sprite = pygame.image.load('Dungeon of Death/New_Sprait/Hero/Tenevoi_mag_parad.png')
-obiekt_sprite = pygame.image.load('Dungeon of Death/New_Sprait/Stena_Dang/Stoun_Sten.png')
-sprite_fon = pygame.image.load('Dungeon of Death/New_Sprait/Pol_Dang/Pol_Level_1.png')
+# Игрок
+player_sprite = pygame.image.load('Dungeon_of_Death/New_Sprait/Hero/Tenevoi_mag_parad.png')
+
+# Стены, пол и двери
+obiekt_sprite = pygame.image.load('Dungeon_of_Death/New_Sprait/Stena_Dang/Stoun_Sten.png')
+sprite_dver = pygame.image.load('Dungeon_of_Death/New_Sprait/Stena_Dang/Stoun_Sten_dver.png')
+staraya_stena = pygame.image.load('Dungeon_of_Death/New_Sprait/Stena_Dang/Stoun_Sten_rasteniya.png')
+
+sprite_fon = pygame.image.load('Dungeon_of_Death/New_Sprait/Pol_Dang/Pol_Level_1.png')
+zamshelii_pol = pygame.image.load('Dungeon_of_Death/New_Sprait/Pol_Dang/Pol_Level_1_Zamshelii.png')
+
+# Декорации
+
+
+# Монстры
+slizen = pygame.image.load('Dungeon_of_Death/New_Sprait/Vragi/Slizen_stoit.png')
+
+# Оружие
+
 
 # Функция для выравнивания позиции к центру клетки
 def align_to_grid(pos_x, pos_y, cell_size):
@@ -65,9 +130,7 @@ def align_to_grid(pos_x, pos_y, cell_size):
  ТОже самое мы делаем для Y.
 '''
 player_size = CELL_SIZE
-initial_player_screen_center_x = WIDTH // 2
-initial_player_screen_center_y = HEIGHT // 2
-player_x, player_y = align_to_grid(initial_player_screen_center_x, initial_player_screen_center_y, CELL_SIZE)
+player_x, player_y = align_to_grid(1 * CELL_SIZE, 8 * CELL_SIZE, CELL_SIZE)
 
 # Статичный объект (в заданной позиции в игровом мире)
 static_object_world_x = 100
@@ -82,13 +145,16 @@ camera_y = 0
 # Переменные управления движением
 direction = None
 move_timer = 0
-move_interval = 500  # Интервал в 500 миллисекунд (0,5 секунды)
+move_interval = 0  # Интервал в 500 миллисекунд (0,5 секунды)
 
 # Игровой цикл
 clock = pygame.time.Clock()
 running = True
 
 while running:
+
+    walls_rect = []
+
     # Внутри цикла отрисовки:
     '''
     for row_index, row in enumerate(level_map): — мы проходим по всей матрице уровня.
@@ -97,19 +163,28 @@ while running:
     Он хранит в себе позицию и размер клетки.
     if cell == 1: ... else: ... — это ядро движка. Программа смотрит на число в матрице.
     '''
-    for row_index, row in enumerate(room_map):
-        for col_index, cell in enumerate(row):
-            # Вычисляем позицию один раз для этой клетки
-            x = col_index * CELL_SIZE + OFFSET_X
-            y = row_index * CELL_SIZE + OFFSET_Y
-            rect = pygame.Rect(x, y, CELL_SIZE, CELL_SIZE)
+    for room_map, OFFSET_X, OFFSET_Y in room:
+        for row_index, row in enumerate(room_map):
+            for col_index, cell in enumerate(row):
+                # Вычисляем позицию один раз для этой клетки
+                x = col_index * CELL_SIZE + OFFSET_X
+                y = row_index * CELL_SIZE + OFFSET_Y
+                rect = pygame.Rect(x, y, CELL_SIZE, CELL_SIZE)
 
-            if cell == 1:
-            # Если в матрице 1 — рисуем красный блок
-                screen.blit(obiekt_sprite, (rect.x - camera_x, rect.y - camera_y))
-            else:
-            # Если в матрице 0 — рисуем белый блок (пустоту)
-                screen.blit(sprite_fon, (rect.x - camera_x, rect.y - camera_y))
+                if cell == 1:
+                # Если в матрице 1 — рисуем красный блок
+                    screen.blit(obiekt_sprite, rect)
+                    walls_rect.append(rect) #для столкновений
+                elif cell == 2:
+                    screen.blit(sprite_dver, rect)
+                elif cell == 3:
+                    screen.blit(staraya_stena, rect)
+                    walls_rect.append(rect)
+                elif cell == 4:
+                    screen.blit(zamshelii_pol, rect)
+                else:
+                # Если в матрице 0 — рисуем белый блок (пустоту)
+                    screen.blit(sprite_fon, rect)
 
     # Обработка событий
     '''
@@ -162,8 +237,17 @@ while running:
         should_move = False: Если позиция игрока совпадает с позицией красного куба,
         переменная should_move устанавливается в False, что означает, что игрок не может перемещаться на эту клетку. 
         '''
-        if potential_player_x == static_object_x and potential_player_y == static_object_y: should_move = False
-        if potential_player_x == statik_pol_x and potential_player_y == statik_pol_y: should_move = True
+        player_rect = pygame.Rect(
+            potential_player_x - CELL_SIZE // 2,
+            potential_player_y - CELL_SIZE // 2,
+            CELL_SIZE,
+            CELL_SIZE
+            )
+        should_move = True
+        for wall in walls_rect:
+            if player_rect.colliderect(wall.inflate(-2, -2)):
+                should_move = False
+                break
 
         # Если движение разрешено, применяем выравнивание по сетке
         if should_move:
@@ -184,25 +268,25 @@ while running:
         direction = None
 
     # Обновление смещения камеры
-    camera_x = player_x - WIDTH // 2 + player_size // 2
-    camera_y = player_y - HEIGHT // 2 + player_size // 2
+#    camera_x = player_x - WIDTH // 2 + player_size // 2
+#    camera_y = player_y - HEIGHT // 2 + player_size // 2
 
     # Отрисовка
-    #screen.blit(sprite_fon, (statik_pol_x - camera_x, statik_pol_y - camera_y))
+    #screen.blit(sprite_fon, (statik_pol_x, statik_pol_y))
 
     # Рисуем статичный объект (на позиции в карте, с учетом смещения камеры)
     '''
     screen.blit(...): Рисует изображение на экране по координатам,
     скорректированным с учетом смещения камеры (camera_x и camera_y).
     '''
-    #screen.blit(obiekt_sprite, (static_object_x - camera_x, static_object_y - camera_y))
+    #screen.blit(obiekt_sprite, (static_object_x, static_object_y))
 
 
     # Отрисовка игрока
     '''
-    screen.blit(...): Рисует игрока на экране с учетом смещения камеры.
+    screen.blit(...): Рисует игрока на экране.
     '''
-    screen.blit(player_sprite, (player_x - camera_x, player_y - camera_y))
+    screen.blit(player_sprite, (player_x - CELL_SIZE // 2, player_y - CELL_SIZE // 2))
 
     # Рисуем сетку
     '''
@@ -212,20 +296,21 @@ while running:
     pygame.draw.line(...): Рисует горизонтальные линии по координате y. 
     CELL_SIZE определяет расстояние между линиями.
     '''
-    for x in range(0, WIDTH, CELL_SIZE):
-        pygame.draw.line(screen, WHITE, (x + CELL_SIZE/2, 0), (x + CELL_SIZE/2, HEIGHT))
-    for y in range(0, HEIGHT, CELL_SIZE):
-        pygame.draw.line(screen, WHITE, (0, y + CELL_SIZE/2), (WIDTH, y + CELL_SIZE/2))
+    for x in range(0, WIDTH + 1, CELL_SIZE):
+        pygame.draw.line(screen, WHITE, (x, 0), (x, HEIGHT))
+    for y in range(0, HEIGHT + 1, CELL_SIZE):
+        pygame.draw.line(screen, WHITE, (0, y), (WIDTH, y))
 
     # Ограничение движения камеры по границам карты
-    map_width = 10000
-    map_height = 10000
+#    map_width = 10000
+#    map_height = 10000
 
-    camera_x = max(0, min(camera_x, map_width - WIDTH))
-    camera_y = max(0, min(camera_y, map_height - HEIGHT))
+#    camera_x = max(0, min(camera_x, map_width - WIDTH))
+#    camera_y = max(0, min(camera_y, map_height - HEIGHT))
 
     # Обновление экрана
     pygame.display.update()
+    screen.fill(BLACK)
     
     # Контроль FPS
     clock.tick(60)
@@ -233,8 +318,3 @@ while running:
 # Завершение игры
 pygame.quit()
 sys.exit()
-
-
-'''
-1) Разобратся с проблемами отрисовки. Никак не разберусь, решаю одну проблему, появляется другая.
-'''
